e71cf811b08508d1548f8a095fec852a09ae49ec
BuildTools <unconfigured@null.spigotmc.org>
2021-06-10 14:48:16 +0900

    commit

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -1,183 +1,268 @@
-﻿// WindowsProject1.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-
-#include "framework.h"
+﻿#include "framework.h"
 #include "WindowsProject1.h"
 
-#define MAX_LOADSTRING 100
+#include <d2d1.h>
+#include <Wincodec.h>
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+#include <cstdlib>
+#include <ctime>
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+#pragma comment(lib, "D2D1.lib")
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+using namespace D2D1;
+
+RECT WndSize;
+
+ID2D1Factory *gp_factory;
+ID2D1HwndRenderTarget *gp_render_target;
+
+ID2D1Bitmap *gp_bitmap;
+
+D2D1_RECT_F g_image_rect;
+
+POINT pre_cursor;
+bool mouse_clicked = false;
+bool is_moving = false;
+bool on_ground = false;
+
+int moving_direction = 1;
+int moving_frame = 0;
+
+int vx=0, vy=0; 
+
+int LoadMyImage(ID2D1RenderTarget* ap_target, const wchar_t* ap_path, enum WICBitmapTransformOptions option)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	if (gp_bitmap != NULL) {
+		gp_bitmap->Release();
+		gp_bitmap = NULL;
+	}
 
-    // TODO: 여기에 코드를 입력합니다.
-    
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WINDOWSPROJECT1, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	IWICImagingFactory *p_wic_factory;
+	HRESULT hr = CoCreateInstance(
+		CLSID_WICImagingFactory, 
+		NULL, 
+		CLSCTX_INPROC_SERVER, 
+		IID_PPV_ARGS(&p_wic_factory));
 
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	IWICBitmapDecoder *p_decoder = NULL;
+	IWICBitmapFrameDecode *p_frame = NULL;
+	IWICBitmapFlipRotator* p_rotator = NULL;
+	IWICFormatConverter *p_converter = NULL;
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT1));
+	hr = p_wic_factory->CreateDecoderFromFilename(ap_path,
+		NULL, 
+		GENERIC_READ, 
+		WICDecodeMetadataCacheOnDemand, 
+		&p_decoder);
 
-    MSG msg;
+	if (SUCCEEDED(hr)) {
+		hr = p_decoder->GetFrame(0, &p_frame);
+	}
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	if (SUCCEEDED(hr)) {
+		hr = p_wic_factory->CreateFormatConverter(&p_converter);
+	}
 
-    return (int) msg.wParam;
+	if (SUCCEEDED(hr)) {
+		hr = p_converter->Initialize(p_frame,
+			GUID_WICPixelFormat32bppPBGRA, 
+			WICBitmapDitherTypeNone, 
+			NULL, 
+			0.0f, 
+			WICBitmapPaletteTypeCustom);
+	}
+
+	if (SUCCEEDED(hr)) {
+		hr = p_wic_factory->CreateBitmapFlipRotator(&p_rotator);
+	}
+
+	if (SUCCEEDED(hr)) {
+		hr = p_rotator->Initialize(p_converter, option);
+	}
+
+	if (SUCCEEDED(hr)) {
+		hr = ap_target->CreateBitmapFromWicBitmap(p_rotator, NULL, &gp_bitmap);
+	}
+
+	if (p_rotator) p_rotator->Release();
+	if (p_converter) p_converter->Release();
+	if (p_frame) p_frame->Release();
+	if (p_decoder) p_decoder->Release();
+	if (p_wic_factory) p_wic_factory->Release();
+
+	return SUCCEEDED(hr);
 }
 
+void CALLBACK OnTimer(HWND hWnd, UINT nMsg, UINT_PTR nIDEvent, DWORD dwTime) {
+	if (nIDEvent == 1) {
+		if (!mouse_clicked) {
+			RECT r;
+			GetWindowRect(hWnd, &r);
 
+			if (WndSize.bottom >= r.bottom + vy) SetWindowPos(hWnd, HWND_TOPMOST, r.left + vx, r.top + vy, 0, 0, SWP_NOSIZE);
+			else {
+				SetWindowPos(hWnd, HWND_TOPMOST, r.left, WndSize.bottom - r.bottom + r.top, 0, 0, SWP_NOSIZE);
+				if (!on_ground) {
+					LoadMyImage(gp_render_target, L"picture/cat_06.png", WICBitmapTransformRotate0);
+					InvalidateRect(hWnd, NULL, TRUE);
+					on_ground = true;
+				}
+			}
+			
+			if (WndSize.left > r.left + vx || WndSize.right < r.right + vx) vx *= -1;
+			if(vy < 30) vy += 1;
+		}
+	}
+	if (nIDEvent == 2) {
+		if (is_moving&&!mouse_clicked) {
+			RECT r;
+			GetWindowRect(hWnd, &r);
 
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
+			if (moving_frame % 25 == 0) {
+				enum WICBitmapTransformOptions option;
+				if (moving_direction == 1) option = WICBitmapTransformFlipHorizontal;
+				if (moving_direction == -1) option = WICBitmapTransformRotate0;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT1));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT1);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+				if (moving_frame % 100 == 0) LoadMyImage(gp_render_target, L"picture/cat_01.png", option);
+				if (moving_frame % 100 == 25) LoadMyImage(gp_render_target, L"picture/cat_02.png", option);
+				if (moving_frame % 100 == 50) LoadMyImage(gp_render_target, L"picture/cat_03.png", option);
+				if (moving_frame % 100 == 75) LoadMyImage(gp_render_target, L"picture/cat_02.png", option);
+				InvalidateRect(hWnd, NULL, TRUE);
+			}
+			if (r.left + moving_direction < WndSize.left) moving_direction = 1;
+			if (r.right + moving_direction > WndSize.right) moving_direction = -1;
+			SetWindowPos(hWnd, HWND_TOPMOST, r.left + moving_direction, r.top, 0, 0, SWP_NOSIZE);
+			if(moving_frame++ == 300) is_moving = false;
+		}
+	}
+	if (nIDEvent == 3) {
+		if (!is_moving) {
+			srand(time(0));
+			if (rand() % 2 == 0) moving_direction = 1;
+			else moving_direction = -1;
+			moving_frame = 0;
+			is_moving = true;
+		}
+	}
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+	if (uMsg == WM_LBUTTONDOWN) {
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+		GetCursorPos(&pre_cursor);
+		SetCapture(hWnd);
+		mouse_clicked = true;
+		on_ground = false;
+	} else if (uMsg == WM_LBUTTONUP) {
 
-   SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
-   SetLayeredWindowAttributes(hWnd, 0, (255 * 70) / 100, LWA_ALPHA);
+		mouse_clicked = false;
+		is_moving = false;
+		ReleaseCapture();
+	} else if (uMsg == WM_MOUSEMOVE) {
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+		if (mouse_clicked) {
+			POINT temp_cursor;
+			GetCursorPos(&temp_cursor);
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+			vx = (temp_cursor.x - pre_cursor.x)/2;
+			vy = (temp_cursor.y - pre_cursor.y)*1.5;
+			
+			RECT r;
+			GetWindowRect(hWnd, &r);
+			if(r.left + temp_cursor.x - pre_cursor.x < WndSize.left) SetWindowPos(hWnd, HWND_TOPMOST, WndSize.left, r.top + temp_cursor.y - pre_cursor.y, 0, 0, SWP_NOSIZE);
+			else if(r.right + temp_cursor.x - pre_cursor.x > WndSize.right) SetWindowPos(hWnd, HWND_TOPMOST, WndSize.right - r.right + r.left, r.top + temp_cursor.y - pre_cursor.y, 0, 0, SWP_NOSIZE);
+			else SetWindowPos(hWnd, HWND_TOPMOST, r.left + temp_cursor.x - pre_cursor.x, r.top + temp_cursor.y - pre_cursor.y, 0, 0, SWP_NOSIZE);
+			pre_cursor = temp_cursor;
+		}
+	} else if (uMsg == WM_PAINT) {
+		ValidateRect(hWnd, NULL);
+		gp_render_target->BeginDraw();
+		gp_render_target->Clear();
+		if (gp_bitmap != NULL) gp_render_target->DrawBitmap(gp_bitmap, &g_image_rect);
+		else gp_render_target->Clear();
+		
+		gp_render_target->EndDraw();
+		return 0;
+	} else if (uMsg == WM_CREATE) {
 
-   return TRUE;
+		SystemParametersInfoA(SPI_GETWORKAREA, 0, &WndSize, 0);
+
+		SetTimer(hWnd, 1, 10, OnTimer);
+		SetTimer(hWnd, 2, 10, OnTimer);
+		SetTimer(hWnd, 3, 10000, OnTimer);
+
+		RECT r;
+		GetClientRect(hWnd, &r);
+
+		gp_factory->CreateHwndRenderTarget(
+			RenderTargetProperties(D2D1_RENDER_TARGET_TYPE_DEFAULT, PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED)),
+			HwndRenderTargetProperties(hWnd, SizeU(r.right,r.bottom)),
+			&gp_render_target);
+
+		g_image_rect.right = (float)r.right;
+		g_image_rect.bottom = (float)r.bottom;
+
+		LoadMyImage(gp_render_target, L"picture/cat_01.png", WICBitmapTransformRotate0);
+		return 0;
+	} else if (uMsg == WM_DESTROY) {
+
+		KillTimer(hWnd, 1);
+		KillTimer(hWnd, 2);
+		KillTimer(hWnd, 3);
+
+		if (gp_render_target != NULL) {
+			gp_render_target->Release();
+			gp_render_target = NULL;
+		}
+
+		PostQuitMessage(0);
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
+	CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+	D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &gp_factory);
 
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	WNDCLASS wc;
+	wchar_t m_class_name[] = L"test";
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = m_class_name;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&wc);
+	/*
+	HWND hWnd = CreateWindow(m_class_name, L"picture",
+		WS_OVERLAPPEDWINDOW, 100, 90, 128, 128, NULL, NULL, hInstance, NULL);
+	SetWindowLong(hWnd, GWL_STYLE, (GetWindowLong(hWnd, GWL_STYLE)&(~WS_OVERLAPPEDWINDOW)|WS_POPUP));
+	*/
+	HWND hWnd = CreateWindow(m_class_name, L"picture",
+		WS_EX_LAYERED | WS_POPUP, 100, 90, 100, 100, NULL, NULL, hInstance, NULL);
+	SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+	SetLayeredWindowAttributes(hWnd, RGB(254, 254, 254), 255, LWA_COLORKEY);
+	
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gp_bitmap != NULL) gp_bitmap->Release();
+	gp_factory->Release();
+	CoUninitialize();
+
+	return (int)msg.wParam;
 }
